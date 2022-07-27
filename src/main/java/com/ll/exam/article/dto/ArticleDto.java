fac53f66722d78649135bbe01ffa0a7e5b86ce40
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-07-27 13:50:49 +0900

    공통 상단,하단 템플릿 만들어서 list.jsp에 적용

--- src/main/java/com/ll/exam/article/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/article/dto/ArticleDto.java
@@ -1,48 +1,13 @@
 package com.ll.exam.article.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ArticleDto {
     private Long id;
     private String title;
     private String body;
-
-
-    public ArticleDto(Long id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleDto{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
 }
--- src/main/java/com/ll/exam/article/dto/ArticleModifyDto.java
+++ src/main/java/com/ll/exam/article/dto/ArticleModifyDto.java
@@ -1,28 +1,15 @@
 package com.ll.exam.article.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ArticleModifyDto {
-    String title;
-    String body;
+    private String title;
+    private String body;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public ArticleModifyDto(String title, String body) {
-        this.title = title;
-        this.body = body;
-    }
 }
